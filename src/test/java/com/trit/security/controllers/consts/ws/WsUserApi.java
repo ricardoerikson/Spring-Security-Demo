9593dbbc6c077c6b9c0ed870a6e6c64105b3b722
Ricardo Erikson <ricardoerikson@gmail.com>
2015-08-28 17:10:06 -0400

    WsUserApi was renamed to WsResourceBuilder and will be added in the next commit

--- src/test/java/com/trit/security/controllers/consts/ws/WsUserApi.java
+++
@@ -1,8 +0,0 @@
-package com.trit.security.controllers.consts.ws;
-
-public class WsUserApi {
-
-    public static final String USERS = "/api/users";
-    public static final String USERS_id = "/api/users/{id}";
-
-}
