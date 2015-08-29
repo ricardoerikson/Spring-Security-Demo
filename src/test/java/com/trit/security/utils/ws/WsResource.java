c9b9ce6437f1cdc2bc5bbf1c9338798a563c6612
Ricardo Erikson <ricardoerikson@gmail.com>
2015-08-29 10:38:58 -0400

    Improvements in WsResource class

--- src/test/java/com/trit/security/utils/ws/WsResource.java
+++ src/test/java/com/trit/security/utils/ws/WsResource.java
@@ -6,18 +6,20 @@ public class WsResource {
     private String id = null;
 
     public WsResource(String resource) {
+        if (resource == null || resource.isEmpty())
+            throw new IllegalArgumentException("Argument 'resource' should be neither 'null' nor empty.");
         this.resource = resource;
     }
 
     public WsResource(String resource, String id) {
-        this.resource = resource;
+        this(resource);
+        if (id == null || id.isEmpty())
+            throw new IllegalArgumentException("Argument 'id' should be neither 'null' nor empty.");
         this.id = id;
     }
 
     @Override
     public String toString() {
-        if (resource == null)
-            return "";
         if (id == null)
             return String.format("/%s", resource);
         return String.format("/%s/%s", resource, id);
